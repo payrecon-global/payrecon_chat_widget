ccd41ec0c59e8850832c03256351040ebdcb46d8
TaufikPayrecon <131147561+TaufikPayrecom@users.noreply.github.com>
2026-04-23 17:15:15 +0800

    zoom enhancements

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
 
 const app  = express();
 const PORT = parseInt(process.env.PORT || '4001');
